232926ac9337a36e3f8a78fbf581e82a3774a9b1
DMoon <dmoon.t@gmail.com>
2019-10-08 16:40:42 +0800

    Add constant folder

---
+++ src/constant/index.js
@@ -0,0 +1,27 @@
+export const LINKS = [
+  { href: "/", name: "首頁" },
+  { href: "/about", name: "關於我們" },
+  { href: "/venue", name: "會場" },
+  { href: "/speaker", name: "講者" },
+  { href: "/agenda", name: "議程" },
+  { href: "/sponsors", name: "合作夥伴" },
+  { href: "/team", name: "工作團隊" },
+  { href: "/conduct", name: "行為準則" },
+]
+
+export const ACTIVITIES = {
+  CONFERENCE: {
+    key: "CONFERENCE",
+    name: "主年會",
+    date: "10/26",
+    title: "JavaScript 穩定之後，然後呢？",
+    location: "富邦國際會議中心",
+  },
+  WORKSHOP: {
+    key: "WORKSHOP",
+    name: "工作坊",
+    date: "9/28",
+    title: "JavaScript 新技術的入門與導入",
+    location: "M Space 共享空間",
+  },
+}
