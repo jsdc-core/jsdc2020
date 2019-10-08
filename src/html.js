8c9b60484828225b3daaa27e72a2cc38dc0ccaf6
DMoon <dmoon.t@gmail.com>
2019-10-08 16:40:23 +0800

    Add custom html.js

---
+++ src/html.js
@@ -0,0 +1,68 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function HTML(props) {
+  function renderGoogleTranslateElementScript() {
+    return (
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+              pageLanguage: 'zh-TW',
+              includedLanguages: 'en,zh-CN,ja,zh-TW',
+              layout: google.translate.TranslateElement.FloatPosition.TOP_RIGHT,
+              multilanguagePage: true,
+            }, 'google_translate_element')
+          }`,
+        }}
+      />
+    )
+  }
+
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+        <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
+        <link rel="icon" type="image/png" href="static/images/jsdc-logo.png" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <noscript key="noscript" id="gatsby-noscript">
+          This app works best with JavaScript enabled.
+        </noscript>
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <script
+          type="text/javascript"
+          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        ></script>
+        {renderGoogleTranslateElementScript()}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
